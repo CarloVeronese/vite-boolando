265f0e63ecda4204adeebc2f503a5c03e3f91279
CarloVeronese <carlo.veronese97@gmail.com>
2023-11-06 14:20:48 +0100

    add - components and style general

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    data() {
+        return {
+            categories: ['Donna', 'Uomo', 'Bambini']
+        }
+    }
+}
+</script>
+
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-1">
+            <ul class="categories-list">
+                <li v-for="section in categories">
+                    <a href="">{{ section }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss">
+
+.categories-list {
+    display: flex;
+    gap: 10px;
+}
+</style>
